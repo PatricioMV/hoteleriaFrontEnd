03820e2c408369426e4c0641621629da0960e396
Patricio Villagra <patriciovillagra94@gmail.com>
2024-07-03 14:21:50 -0300

    Added Header and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,36 @@
-function Footer() {
-  return <h6 style={{ color: 'grey' }}>ACA VA EL FOOTER</h6>;
-}
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      <Container>
+        <Row>
+          <Col md="4">
+            <h5>Stanley Hotel</h5>
+            <p>Your comfort is our priority.</p>
+          </Col>
+          <Col md="4">
+            <h5>Contact Information</h5>
+            <p>Email: <a href="mailto:patriciovillagra94@gmail.com" className="text-white">patriciovillagra94@gmail.com</a></p>
+            <p>GitHub: <a href="https://github.com/PatricioMV" target="_blank" rel="noopener noreferrer" className="text-white">PatricioMV</a></p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/patriciomvillagra/" target="_blank" rel="noopener noreferrer" className="text-white">Patricio Villagra</a></p>
+          </Col>
+          <Col md="4">
+            <h5>Useful Links</h5>
+            <p><a href="/reservations" className="text-white">Reservations</a></p>
+            <p><a href="/terms" className="text-white">Terms and Conditions</a></p>
+            <p><a href="/privacy" className="text-white">Privacy Policy</a></p>
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col>
+            <p>© {new Date().getFullYear()} Stanley Hotel. All rights reserved. (Just kidding, feel free to copy!)</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import Home from '../components/Home';
+import Payments from '../components/Payments';
+import Reservations from '../components/Reservations';
+
 function Header() {
-  return <h6 style={{ color: 'grey' }}>ACA VA EL HEADER</h6>;
+  return (
+    <Router>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Hotel Management System
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/payments">
+                Payments
+              </Nav.Link>
+              <Nav.Link as={Link} to="/reservations">
+                Reservations
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/payments" element={<Payments />} />
+          <Route path="/reservations" element={<Reservations />} />
+        </Routes>
+      </Container>
+    </Router>
+  );
 }
 
 export default Header;
