866d4ffaccc03cb0c75f2c1f8898bb5ca44a0f87
Patricio Villagra <patriciovillagra94@gmail.com>
2024-07-23 10:40:05 -0300

    Added hotel logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppRouter from './router/AppRouter';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
